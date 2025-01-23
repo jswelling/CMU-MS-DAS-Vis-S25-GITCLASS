de75dec3f7a249af6ae4b5b2381d35b8b22e18de
kkirpa <kirpa@vt.edu>
2025-01-23 12:10:23 -0500

    merged

--- src/helloworld/helloworld.py
+++ src/helloworld/helloworld.py
@@ -4,6 +4,11 @@ def main():
     print("""
     Hello World! from:
     -Joel Welling
+    -Alekhya Vittalam
+    -Kevin Love
+    -Ananya Chembai
+    -Aswathama Shnamugam Marimuthu
+    -Kirpa Kaur
     """)
 
 if __name__ == "__main__":
